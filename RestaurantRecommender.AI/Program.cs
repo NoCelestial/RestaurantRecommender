dc980e007a861a364e89eb07f0e03184cb620ef9
AmirAliShokri <amirali.shokri12@gmail.com>
2021-04-09 14:21:33 +0430

    Initial Commit

---
+++ RestaurantRecommender.AI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RestaurantRecommender.AI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
