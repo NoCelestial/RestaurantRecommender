b56e90fdc3ea9f0e0ac405f51d4c309d507bd322
AmirAliShokri <amirali.shokri12@gmail.com>
2021-04-09 15:40:24 +0430

    Add Files To Projeect

---
+++ RestaurantRecommender.AI/ModelInput.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ML.Data;
+
+namespace RestaurantRecommender.AI
+{
+    public class ModelInput
+    {
+        [LoadColumn(0)]
+        public string UserId { get; set; }
+        [LoadColumn(1)]
+        public string RestaurantName { get; set; }
+        [LoadColumn(2)]
+        public float TotalRating { get; set; }
+    }
+}
