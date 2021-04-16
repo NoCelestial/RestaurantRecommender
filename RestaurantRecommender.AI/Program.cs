d4f756edbec9b7a29054134b75cd9b5223c697b9
AmirAliShokri <amirali.shokri12@gmail.com>
2021-04-16 15:07:34 +0430

    Add Recommender And Options:

--- RestaurantRecommender.AI/Program.cs
+++ RestaurantRecommender.AI/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.IO;
+using Microsoft.ML;
+using Microsoft.ML.Trainers;
+using RestaurantRecommender.AI.Helper;
+using SHA.BeautifulConsoleColor.Core.Class;
+using SHA.BeautifulConsoleColor.Core.Model;
 
 namespace RestaurantRecommender.AI
 {
@@ -6,7 +12,29 @@ namespace RestaurantRecommender.AI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            BCCConsole.Write(BCCConsoleColor.DarkBlue,false,"Restaurant Recommender Is Started . . .");
+
+            MLContext mlContext = new MLContext(0);
+            var trainingDataFile = Environment.CurrentDirectory + @"\Data\TrainingFile.tsv";
+            DataPreparer.PreprocessData(trainingDataFile);
+            IDataView trainingDataView = mlContext.Data
+                .LoadFromTextFile<ModelInput>(trainingDataFile,hasHeader:true);
+
+            var dataPreProcessingPipeLine = mlContext.Transforms.Conversion
+                .MapValueToKey("UserIdEncoded",nameof(ModelInput.UserId))
+                .Append(mlContext.Transforms.Conversion
+                    .MapValueToKey("RestaurantNameEncoded",nameof(ModelInput.RestaurantName)));
+
+			var options = new MatrixFactorizationTrainer.Options
+			{
+				MatrixColumnIndexColumnName = "UserIdEncoded",
+				MatrixRowIndexColumnName = "RestaurantNameEncoded",
+				LabelColumnName = "TotalRating",
+				NumberOfIterations = 100,
+				ApproximationRank = 100,
+                Quiet = true
+			};
+
+		}
     }
 }
